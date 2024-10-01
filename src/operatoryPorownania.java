0ac355fdf8d2d4c410dff5e23b10daa7ea2b55c6
Gatossa <email@users.noreply.github.com>
2024-10-01 12:45:03 +0200

    added

---
+++ src/operatoryPorownania.java
@@ -0,0 +1,19 @@
+public class operatoryPorownania
+{
+    public static void main(String[] args) {
+        int firstNumber=4;
+        int secondNumber=6;
+
+        boolean result= firstNumber > secondNumber;
+
+        System.out.println(result);
+        System.out.println(firstNumber > secondNumber);
+        System.out.println(firstNumber < secondNumber);
+        System.out.println(firstNumber >= secondNumber);
+        System.out.println(firstNumber <= secondNumber);
+        System.out.println(firstNumber == secondNumber);
+        System.out.println(firstNumber != secondNumber);
+
+    }
+
+}
