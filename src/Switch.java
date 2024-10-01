ecc91bcc2841186489ec6a7922819b177f497260
Gatossa <email@users.noreply.github.com>
2024-10-01 14:47:54 +0200

    added

---
+++ src/Switch.java
@@ -0,0 +1,22 @@
+public class Switch {
+    public static void main(String[] args) {
+        String dish = "Pizza";
+
+        switch (dish){
+            case "Pizza":
+                System.out.println("Cena to 22 PLN");
+                break;
+            case "Łosoś":
+                System.out.println("Cena to 42 PLN");
+                break;
+            case "Frytki":
+                System.out.println("Cena to 12 PLN");
+                break;
+            default:
+                System.out.println("Nie ma, idź gdzie indziej");
+        }
+    }
+
+    private static class string {
+    }
+}
