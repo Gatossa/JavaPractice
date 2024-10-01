c11efaadee0adeced9054fbf3f8786fdb435cc87
Gatossa <email@users.noreply.github.com>
2024-10-01 18:18:38 +0200

    added

---
+++ src/studentref.java
@@ -0,0 +1,14 @@
+public class studentref {
+        public String imie;
+        public String nazwisko;
+        public String email;
+        public int index;
+
+        public void przedstawsie() {
+            System.out.println("Nazywam się "+ imie+ " "  + nazwisko);}
+        public void podajemail() {
+            System.out.println("Mój email to " + email);}
+        public void podajindex() {
+                    System.out.println("Mój numer indeksu to " + index);}
+
+                }
