0e9c6aa80a8f94b5ce6c631d6448c0af4493987c
Gatossa <email@users.noreply.github.com>
2024-10-01 14:23:16 +0200

    added

---
+++ src/zadanie2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class zadanie2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proszę podaj swój wiek");
+        int wiek = scanner.nextInt();
+        if (wiek < 18 && wiek >= 0) {
+            System.out.println("Nie mozesz kupić alkoholu");
+        } else if (wiek < 0) {
+            System.out.println("Wprowadź poprawną wartość");
+        } else {
+            System.out.println("Chlej do woli");
+        }
+    }
+}
+
