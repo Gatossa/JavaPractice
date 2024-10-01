6b9eeb79066de528552a07ba3fd6e17022b15c6a
Gatossa <email@users.noreply.github.com>
2024-10-01 14:23:23 +0200

    added

---
+++ src/instrukcjeWarunkowe.java
@@ -0,0 +1,9 @@
+public class instrukcjeWarunkowe {
+
+    public static void main(String[] args) {
+         float a=10;
+         float b=12;
+         if(b!=0)
+        System.out.println("Wynik dzielenia to " + a / b);
+         }
+}
