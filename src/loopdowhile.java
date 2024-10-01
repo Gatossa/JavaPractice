17c0241d14a7e79fcd5ccf9f8d47c296f6f70dc0
Gatossa <email@users.noreply.github.com>
2024-10-01 15:51:57 +0200

    added

---
+++ src/loopdowhile.java
@@ -0,0 +1,14 @@
+public class loopdowhile {
+
+    public static void main(String[] args) {
+
+
+        int numb = 5;
+        do {
+            System.out.println("Moja wartosc to " +numb+ " i jestem mniejszy od 20");
+            numb++;
+        }
+        while (numb<20);
+
+    }
+}
