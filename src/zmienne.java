9c211c3d1b8530e92a36b1610d37ad04088e6699
Gatossa <email@users.noreply.github.com>
2024-10-01 12:10:56 +0200

    homework added

---
+++ src/zmienne.java
@@ -0,0 +1,12 @@
+public class zmienne {
+public static void main(String[] args) {
+    String     Nazwa=  "Agata";
+    int firstNumber, secondNumber, result;
+    firstNumber = 1;
+    secondNumber = 2;
+    result = firstNumber+secondNumber;
+
+    System.out.println(result);
+    System.out.println(Nazwa);
+}
+}
