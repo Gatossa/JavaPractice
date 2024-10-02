261a075ed2aa6645fff1b54d6146ddddbbc973c5
Gatossa <email@users.noreply.github.com>
2024-10-02 15:02:29 +0200

    added

---
+++ src/User.java
@@ -0,0 +1,23 @@
+public class User {
+
+    public String username;
+    public String password;
+
+    /*public User(){};
+     public User(String user, String pass) {
+         System.out.println("Hello z kontruktora");
+         username=user;
+         password=pass;*/
+
+     public User(String username, String password) {
+         System.out.println("Hello z kontruktora");
+         this.username=username;
+         this.password=password;
+
+
+     }
+
+    public void sayHello(){
+        System.out.println("Hello, my name is " + username);
+    }
+}
---
+++ src/UserTest.java
@@ -0,0 +1,15 @@
+public class UserTest {
+
+    public static void main(String[] args) {
+        //User user= new User();
+        User user=new User("adam", "hello");
+
+        //user.username= "adam";
+        //user.password= "qwerty";
+
+
+        System.out.println(user.username);
+        System.out.println(user.password);
+        user.sayHello();
+    }
+}
